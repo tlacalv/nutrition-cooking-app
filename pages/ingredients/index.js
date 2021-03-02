6fdcec5b886282e9308de4b3a9abfe4fd81b4dd6
Tlacaelel <radeon777@live.com>
2021-03-02 11:58:30 -0700

    Created base layout for application

---
+++ pages/ingredients/index.js
@@ -0,0 +1,16 @@
+import Layout from '../../components/layout'
+import Head from 'next/head'
+
+export default function Ingredients() {
+  return (
+    <Layout>
+      <Head>
+        <title>Ingredients - Nutrition cooking</title>
+      </Head>
+      <div>
+        holo
+      </div>
+
+    </Layout>
+  )
+}
