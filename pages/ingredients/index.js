1588885d4d9d55e8b87f74049b7a0b16154f4907
Tlacaelel <radeon777@live.com>
2021-03-16 08:56:18 -0700

    Implemented search functionality on ingredients page

--- pages/ingredients/index.js
+++ pages/ingredients/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { useAuth } from "../../contexts/AuthContext";
 import SearchBar from "../../components/SearchBar";
 import useSWR from "swr";
+import { useState } from "react";
 import { Row, Button } from "antd";
 import styles from "../../styles/list.module.css";
 import IngredientCard from "../../components/IngredientCard";
@@ -10,30 +11,55 @@ import SkeletonList from "../../components/SkeletonList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
+import debounce from "lodash/debounce";
 
 export default function Ingredients() {
   const router = useRouter();
   const { currentUser, JWT } = useAuth();
+  const [query, setQuery] = useState("");
   const { data: ingredients, error } = useSWR(
     !currentUser ? false : ["ingredients", JWT]
   );
+  const { data: ingredientsQueried, errorQuery } = useSWR(
+    !query ? false : [`ingredients/search/?queryString=${query}`, JWT]
+  );
+
+  function search(e) {
+    setQuery(e.target.value);
+  }
   return (
     <Layout>
       <Head>
         <title>Ingredients - Nutrition cooking</title>
       </Head>
-      <SearchBar
-        search={(e) => {
-          console.log("search ingredient", e.target.value);
-        }}
-      />
+      <SearchBar search={debounce(search, 1000)} />
 
       <Row gutter={8} className={styles.list} justify="center">
-        {!(ingredients || error) ? (
+        {!query ? (
+          !(ingredients || error) ? (
+            <SkeletonList elements={20} />
+          ) : (
+            <>
+              {ingredients.data.map((item) => (
+                <IngredientCard key={item._id} ingredient={item} />
+              ))}
+
+              <Button
+                type="primary"
+                className={styles.addBtn}
+                icon={<FontAwesomeIcon icon={faPlus} />}
+                shape="circle"
+                onClick={() => {
+                  router.push("/ingredients/add");
+                }}
+              ></Button>
+            </>
+          )
+        ) : !(ingredientsQueried || errorQuery) ? (
           <SkeletonList elements={20} />
         ) : (
           <>
-            {ingredients.data.map((item) => (
+            {ingredientsQueried.data.map((item) => (
               <IngredientCard key={item._id} ingredient={item} />
             ))}
 
