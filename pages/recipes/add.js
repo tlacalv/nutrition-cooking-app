645c4cf16893ab3f0ba089229df57c151edaa374
Tlacaelel <radeon777@live.com>
2021-03-17 10:56:49 -0700

    Made request of search to the API

--- pages/recipes/add.js
+++ pages/recipes/add.js
@@ -1,29 +1,31 @@
 import ViewLayout from "../../components/ViewLayout";
 import Head from "next/head";
-import {
-  Form,
-  Row,
-  Col,
-  Input,
-  Button,
-  Alert,
-  InputNumber,
-} from "antd";
+import { Form, Row, Col, Input, Button, Space, Alert, InputNumber } from "antd";
 import styles from "../../styles/viewlayout.module.css";
 import Api from "../../utils/api";
 import { useAuth } from "../../contexts/AuthContext";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
-import { nonZero, positiveNumberVal } from '../../utils/validation';
-
+import useSWR from 'swr';
+import { nonZero, positiveNumberVal } from "../../utils/validation";
+import debounce from 'lodash/debounce';
 
 export default function add() {
   const router = useRouter();
+  const searchRef = useRef();
   const { JWT } = useAuth();
   const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [ingredientList, setIngredientList] = useState([]);
+  const { data: ingredientsQueried, errorQuery } = useSWR(
+    !query ? false : [`ingredients/search/?queryString=${query}`, JWT]
+  );
 
   const [error, setError] = useState("");
- 
+  console.log(ingredientsQueried)
+  function search() {
+    setQuery(searchRef.current.input.value)
+  }
   return (
     <ViewLayout title="Add" subTitle="Recipes">
       <Head>
@@ -32,7 +34,20 @@ export default function add() {
       <form>
         {error && <Alert message={error} type="error" showIcon />}
         <br></br>
-        
+        <Row gutter={[16, 16]}>
+          <Col span={24}>
+            <Input placeholder="Name" />
+          </Col>
+          <Col span={24}>
+            <Input
+              type="search"
+              size="small"
+              ref={searchRef}
+              onChange={debounce(search, 500)}
+              placeholder="Search for ingredient"
+            />
+          </Col>
+        </Row>
       </form>
     </ViewLayout>
   );
