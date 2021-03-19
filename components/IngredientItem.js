9fe231b98061f2c020de98609159a3933d8d16d1
Tlacaelel <radeon777@live.com>
2021-03-19 10:19:18 -0700

    Created ingredient item component

---
+++ components/IngredientItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Button, Typography, InputNumber } from "antd";
+import styles from '../styles/recipes.module.css';
+
+export default function IngredientItem(props) {
+  return (
+    <div className={styles.ingredient_item}>
+      <Button
+        shape="circle"
+        onClick={props.remove}
+        type="text"
+        icon={<FontAwesomeIcon icon={faTimes} />}
+      />
+      <Typography.Text className={styles.ingredient_name} strong>{props.name}</Typography.Text>
+      <div className={styles.ingredient_input}>
+        <Typography.Text className={styles.label_input}>Quantity</Typography.Text>
+        <InputNumber style={{width:'100%'}} size="small" min={0} onChange={props.edit} defaultValue={0}></InputNumber>
+
+      </div>
+    </div>
+  );
+}
--- pages/recipes/add.js
+++ pages/recipes/add.js
@@ -1,12 +1,22 @@
 import ViewLayout from "../../components/ViewLayout";
 import Head from "next/head";
-import { Form, Row, Col, Input, Button, Skeleton, Alert, InputNumber } from "antd";
+import {
+  Form,
+  Row,
+  Col,
+  Input,
+  Button,
+  Skeleton,
+  Alert,
+  InputNumber,
+} from "antd";
 import Api from "../../utils/api";
 import styles from "../../styles/recipes.module.css";
 import { useAuth } from "../../contexts/AuthContext";
 import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import IngredientItem from "../../components/IngredientItem";
 import { nonZero, positiveNumberVal } from "../../utils/validation";
 import debounce from "lodash/debounce";
 import SearchElement from "../../components/SearchElement";
@@ -23,10 +33,10 @@ export default function add() {
   );
 
   const [error, setError] = useState("");
-  //effects 
-  useEffect(()=>{
-    console.log(ingredientList)
-  },[ingredientList])
+  //effects
+  useEffect(() => {
+    console.log(ingredientList);
+  }, [ingredientList]);
   //functions
   function search() {
     setQuery(searchRef.current.input.value);
@@ -34,10 +44,10 @@ export default function add() {
   function blur() {
     setQuery("");
     searchRef.current.state.value = "";
-    searchRef.current.input.value = ""
+    searchRef.current.input.value = "";
   }
   function addIngredient(item) {
-    setIngredientList(prevState=>[...prevState,item])
+    setIngredientList((prevState) => [...prevState, item]);
   }
   return (
     <ViewLayout title="Add" subTitle="Recipes">
@@ -55,35 +65,56 @@ export default function add() {
             <div className={styles.search} tabIndex="20">
               <Input
                 type="search"
-                onBlur={debounce(blur,500)}
+                onBlur={debounce(blur, 500)}
                 size="small"
                 ref={searchRef}
                 allowClear
                 onChange={debounce(search, 500)}
                 placeholder="Search for ingredient"
               />
-              
+
               {query &&
                 (!ingredientsQueried ? (
                   <div className={styles.skeleton_list}>
-                    <Skeleton.Input size="small" className={styles.skeleton_item} active />
-                    <Skeleton.Input size="small" className={styles.skeleton_item} active />
-                    <Skeleton.Input size="small" className={styles.skeleton_item} active />
+                    <Skeleton.Input
+                      size="small"
+                      className={styles.skeleton_item}
+                      active
+                    />
+                    <Skeleton.Input
+                      size="small"
+                      className={styles.skeleton_item}
+                      active
+                    />
+                    <Skeleton.Input
+                      size="small"
+                      className={styles.skeleton_item}
+                      active
+                    />
                   </div>
                 ) : (
                   <ul className={styles.list}>
-                    {
-                      ingredientsQueried.data.map((item,index)=><SearchElement key={index} name={item.name} onClick={()=>addIngredient(item)} />)
-                      
-                    }
-                    {
-                      ingredientsQueried.data.length === 0 ? <li className={styles.no_results}>no recipes found</li>: ''
-                    }
+                    {ingredientsQueried.data.map((item, index) => (
+                      <SearchElement
+                        key={index}
+                        name={item.name}
+                        onClick={() => addIngredient(item)}
+                      />
+                    ))}
+                    {ingredientsQueried.data.length === 0 ? (
+                      <li className={styles.no_results}>no recipes found</li>
+                    ) : (
+                      ""
+                    )}
                   </ul>
                 ))}
             </div>
           </Col>
+          <Col span={24}>Ingredient list</Col>
           <Col span={24}>
+            {ingredientList.map((ingredient, index) => {
+              return <IngredientItem name={ingredient.name} />
+            })}
           </Col>
         </Row>
       </form>
