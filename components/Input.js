2321aae7e170505ebea6c965c4a150c8be3501c1
Tlacaelel <radeon777@live.com>
2021-04-12 10:34:59 -0600

    Formated code and implemented support to type number

--- components/Input.js
+++ components/Input.js
@@ -1,7 +1,14 @@
 import styles from "../styles/sass/components/input.module.scss";
 import { classList } from "../functions";
 
-export default function Input({small, label, error, touched, ...props}) {
+export default function Input({
+  small,
+  label,
+  error,
+  touched,
+  type,
+  ...props
+}) {
   const containerClasses = classList({
     [styles.input_container]: true,
     [styles.small]: small,
@@ -15,23 +22,28 @@ export default function Input({small, label, error, touched, ...props}) {
     "rg-16": true,
     [styles.label]: true,
     [styles.label_active]: !!props.value,
-    [styles.small_label] : small,
+    [styles.small_label]: small,
   });
-  const handleChange = (e, cb)=> {
-    cb?.(e)
-  }
+  const handleChange = (e, cb) => {
+    if (type === "number") {
+      let value = e.target.value;
+      if (isNaN(value)) return;
+    }
+    if (e.target.value) e.target.value = parseInt(e.target.value, 10);
+    cb?.(e);
+  };
 
   return (
     <div className={containerClasses}>
       <label>
         <input
           {...props}
-          onChange={(e)=>handleChange(e, props.onChange)}
+          onChange={(e) => handleChange(e, props.onChange)}
           className={inputClasses}
         />
         <span className={labelClasses}>{label}</span>
       </label>
-      {error && touched ? (<div className={styles.error}>{error}</div>):null}
+      {error && touched ? <div className={styles.error}>{error}</div> : null}
     </div>
   );
 }
