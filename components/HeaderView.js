4f451ef4c4f602aa3233eba2852d41a980e2e6aa
Tlacaelel <radeon777@live.com>
2021-04-15 09:16:45 -0600

    Implemented an alternate look to headerview

--- components/HeaderView.js
+++ components/HeaderView.js
@@ -2,18 +2,26 @@ import React from 'react'
 import styles from '../styles/sass/components/headerview.module.scss';
 import BackButton from './BackButton';
 import {classList} from '../functions'
-const classesTitle = classList({
-  'rg-26': true,
-  [styles.title]: true
-})
-const classesSubtitle = classList({
-  'rg-20': true,
-  [styles.subtitle]: true
-})
-export default function HeaderView(props) {
 
+export default function HeaderView(props) {
+  const {secondary} = props;
+
+  const classesTitle = classList({
+    'rg-26': true,
+    [styles.title]: true,
+    [styles.title_secondary]: secondary
+  })
+  const classesHeader = classList({
+    [styles.header]: true,
+    [styles.header_secondary]: secondary
+  })
+  const classesSubtitle = classList({
+    'rg-20': true,
+    [styles.subtitle]: true,
+    [styles.subtitle_secondary]: secondary
+  })
   return (
-    <div className={styles.header}>
+    <div className={classesHeader}>
       <BackButton />
       <h1 className={classesTitle}>{props.title}</h1>
       <h3 className={classesSubtitle}>{props.subtitle}</h3>
