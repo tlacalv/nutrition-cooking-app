fcbd0d54132d0dfbf066210dd923fb81f9b6e393
Tlacaelel <radeon777@live.com>
2021-04-19 08:34:38 -0600

    Round information

--- components/InformationLabels.js
+++ components/InformationLabels.js
@@ -7,22 +7,22 @@ export default function InformationLabels({calories, fat, carbohydrate, protein}
   return (
     <div className={styles.info}>
       <div className={styles.bubble}>
-        <div className={`${styles.quantity} ${styles.accent} md-14`}>{calories}</div>
+        <div className={`${styles.quantity} ${styles.accent} md-14`}>{Math.round(calories)}</div>
         <div className={`${styles.unit} md-12`}>kcal</div>
         <div className={`${styles.unit_name} rg-10`}>Calories</div>
       </div>
       <div className={styles.bubble}>
-        <div className={`${styles.quantity} md-14`}>{fat}</div>
+        <div className={`${styles.quantity} md-14`}>{Math.round(fat)}</div>
         <div className={`${styles.unit} md-12`}>gr</div>
         <div className={`${styles.unit_name} rg-10`}>Fat</div>
       </div>
       <div className={styles.bubble}>
-        <div className={`${styles.quantity} md-14`}>{carbohydrate}</div>
+        <div className={`${styles.quantity} md-14`}>{Math.round(carbohydrate)}</div>
         <div className={`${styles.unit} md-12`}>gr</div>
         <div className={`${styles.unit_name} rg-10`}>Carbohydrate</div>
       </div>
       <div className={styles.bubble}>
-        <div className={`${styles.quantity} md-14`}>{protein}</div>
+        <div className={`${styles.quantity} md-14`}>{Math.round(protein)}</div>
         <div className={`${styles.unit} md-12`}>gr</div>
         <div className={`${styles.unit_name} rg-10`}>Protein</div>
       </div>
