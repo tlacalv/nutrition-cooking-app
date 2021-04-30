5df632ca376ab8852b4aab204ced938e15491281
Tlacaelel <radeon777@live.com>
2021-04-30 12:55:59 -0600

    Implemented landing page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
+import styles from '../styles/sass/landing.module.scss';
+import Button from '../components/Button';
+import Image from 'next/image';
+import {useRouter} from 'next/router';
 
 export default function HomePage() {
-  return <div>Welcome to nutrition app</div>
+  const router = useRouter();
+  return (
+    <div className={styles.bg}>
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <Image src="/logo-vertical.svg" width={200} height={200} />
+        </div>
+        <div className={styles.buttons}>
+          <Button onClick={()=>router.push('/login')} >Log In</Button>
+          <Button onClick={()=>router.push('/signup')} >Sign Up</Button>
+        </div>
+      </div>
+    </div>
+    )
 }
