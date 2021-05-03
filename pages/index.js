848aee80b5b7df6f7dce3ce28d7bd641eed38e07
Tlacaelel <radeon777@live.com>
2021-05-03 08:07:09 -0600

    Added title to home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import styles from '../styles/sass/landing.module.scss';
 import Button from '../components/Button';
 import Image from 'next/image';
+import Head from 'next/head';
 import {useRouter} from 'next/router';
 
 export default function HomePage() {
   const router = useRouter();
   return (
     <div className={styles.bg}>
+      <Head>
+        <title>Nutrition cooking</title>
+      </Head>
       <div className={styles.content}>
         <div className={styles.logo}>
           <Image src="/logo-vertical.svg" width={200} height={200} />
